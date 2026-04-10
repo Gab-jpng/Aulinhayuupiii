83a9ad385a8d26039b226e1322f0785c8df3cb32
Gabriella Burei Van Haandel <gabilindinhavh19@gmail.com>
2026-04-10 11:29:49 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Aulinhayuupiii.01
-Projeto proposto pelo drade
+# parte_2
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
+
+This project was created using `bun init` in bun v1.3.11. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
--- core.ts
+++ core.ts
@@ -1,21 +1,47 @@
-//lógica principal da aplicação
-const filename = __dirname + '/data.todo.json';
-let list: string[] | null = null!;
+const jsonFilePath = __dirname + '/data.todo.json';
+const list: string[] = await loadFromFile();
 
 async function loadFromFile() {
-    if (list !== null) 
-        return
-    try {
-        const file = Bun.file(filename);
-        const content = await file.text();
-        list = JSON.parse(content) as string[];
-    } catch (error) {
-        Bun.write(filename, "[]");
-        list = [];
-    }
-    
+  try {
+    const file = Bun.file(jsonFilePath);
+    const content = await file.text();
+    return JSON.parse(content) as string[];
+  } catch (error: any) {
+    if (error.code === 'ENOENT')
+      return [];
+    throw error;
+  }
 }
 
-async function SaveToFile() {
-    await Bun.write(filename, JSON.stringify(list));
+async function saveToFile() {
+  try {
+    await Bun.write(jsonFilePath, JSON.stringify(list));
+  } catch (error: any) {
+   throw new Error("Erro ao salvar os dados no arquivo: " + error.message);
+  }
 }
+
+async function addItem(item: string) {
+  list.push(item);
+  await saveToFile();
+}
+
+async function getItems() {
+  return list;
+}
+
+async function updateItem(index: number, newItem: string) {
+  if (index < 0 || index >= list.length)
+    throw new Error("Index fora dos limites");
+  list[index] = newItem;
+  await saveToFile();
+}
+
+async function removeItem(index: number) {
+  if (index < 0 || index >= list.length)
+    throw new Error("Index fora dos limites");
+  list.splice(index, 1);
+  await saveToFile();
+}
+
+export default { addItem, getItems, updateItem, removeItem };
